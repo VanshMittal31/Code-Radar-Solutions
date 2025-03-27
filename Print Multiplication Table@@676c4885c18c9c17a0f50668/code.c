2ecf4ae4b75dff7a8f27eebd9f5ac6fa66bfcc59
Vansh <vm989923@gmail.com>
2025-03-27 17:02:36 +0530

    Solution to Print Multiplication Table

--- Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
+++ Print Multiplication Table@@676c4885c18c9c17a0f50668/code.c
@@ -1,11 +1,14 @@
-# include <stdio.h>
-int main (){
-    int  a,b;
-    // b =1;
+#include <stdio.h>
+int main()
+{
+    int a;
+    printf("enter a number");
     scanf("%d",&a);
+    int b =1;
     for (int i=a;i<=a*10;i=i+a){
-        // b = b*i;
-        printf("%d x %d = %d\n",a,i,i);
+        
+        printf("%d x %d = %d \n",a,b,i);
+        b++;
     }
     return 0;
 }
